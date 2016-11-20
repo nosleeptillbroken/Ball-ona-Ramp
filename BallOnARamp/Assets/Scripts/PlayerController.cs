4aa369c0097b5e956e7ef0cf99d9ba60d75aa13e
jthra037 <its_thrasher@live.ca>
2016-11-20 02:17:36 -0500

    Add PlayerController.cs

---
+++ BallOnARamp/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour {
+
+//    LinkedList<>
+/*
+    void Update()
+    {
+        // If the player hit space, do the thing
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+    */
+}
