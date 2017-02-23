e02749dede74bde42230bae31afedb6d93510f92
Andrew MacMillan <andrewmacmilan1@hotmail.com>
2017-02-23 13:46:53 -0500

    eod. started working on menu

---
+++ BallOnARamp/Assets/Scripts/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    private static int numElements = 5;
+    [SerializeField] private GameObject[] MenuObjects = new GameObject[numElements];
+	// Use this for initialization
+
+  
+    public void StartPressed()
+    {
+        for (int i = 0; i < numElements; i++)
+        {
+            MenuObjects[i].GetComponent<CanvasScaler>().scaleFactor = 2;
+        }
+    }
+}
